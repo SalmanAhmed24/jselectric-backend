b0a77c5dbd0e2aff844d966a95131cb7e4129894
SalmanAhmed24 <salman.ahmed.abbasi.24@gmail.com>
2023-11-23 15:37:09 +0500

    restored the end points

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://jselectric.vercel.app/",
   },
 });
 io.on("connection", (socket) => {
